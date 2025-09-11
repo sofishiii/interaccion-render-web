0c574ccac7c1658be88678c2aa5d808757a3379a
sofishi <sislas@centro.edu.mx>
2025-09-11 10:42:39 -0600

    correcciones

--- S06-Tareas/app.js
+++ S06-Tareas/app.js
@@ -1,28 +1,28 @@
-console.log('sesión 05. Tareas - MATCAPS');
+console.log('sesión 06. Tareas - MATCAPS');
 console.log(THREE);
 
-// Obtener canvas
+
 const canvas = document.getElementById('lienzo');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Escena
+
 const scene = new THREE.Scene();
 
-// Cámara
+
 const camera = new THREE.PerspectiveCamera(45, canvas.width / canvas.height, 0.1, 1000);
 camera.position.z = 10;
 
-// Texturas
+
 const textureLoader = new THREE.TextureLoader();
 
-// Materiales y geometrías
+
 let trunkMesh, crownMesh;
 
 const trunkGeometry = new THREE.CylinderGeometry(0.5, 0.5, 3, 32); // Tronco
 const crownGeometry = new THREE.BoxGeometry(2, 2, 2);
 
-// Cargar textura para el tronco (matcap2)
+
 textureLoader.load('./assets/matcap2.png', function (matcapTexture2) {
     const trunkMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture2 });
 
@@ -30,15 +30,15 @@ textureLoader.load('./assets/matcap2.png', function (matcapTexture2) {
     trunkMesh.position.y = -1.5; // Colocar parte inferior (tronco)
     scene.add(trunkMesh);
 
-    // Ahora cargamos la copa
+    // Ahora carga la copa
     textureLoader.load('./assets/matcap3.png', function (matcapTexture3) {
         const crownMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture3 });
 
         crownMesh = new THREE.Mesh(crownGeometry, crownMaterial);
-        crownMesh.position.y = 1.5; // Colocar encima del tronco
+        crownMesh.position.y = 1.5; // Colocar parte superior (copa)
         scene.add(crownMesh);
 
-        // Iniciar animación cuando ambos objetos estén listos
+
         animate();
     },
     undefined,
@@ -52,15 +52,15 @@ function (error) {
     console.error('Error cargando matcap2.png:', error);
 });
 
-// Renderer
+
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(canvas.width, canvas.height);
 
-// Animación
+
 function animate() {
     requestAnimationFrame(animate);
 
-    // Rotar ambas partes del "árbol"
+
     if (trunkMesh) {
         trunkMesh.rotation.y += 0.01;
     }
