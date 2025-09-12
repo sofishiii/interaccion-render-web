20b73eaf58190fdf8a6bf2bf10de855535b3b9a2
sofishi <sislas@centro.edu.mx>
2025-09-12 13:21:05 -0600

    correcciones tarea5

--- S99-Tareas/05-Tarea/app.js
+++ S99-Tareas/05-Tarea/app.js
@@ -1,10 +1,8 @@
-// Selecciona el canvas del DOM
-const canvas = document.getElementById('canvas');
 
-// Escena
+const canvas = document.getElementById('canvas');
 const scene = new THREE.Scene();
 
-// Cámara
+
 const camera = new THREE.PerspectiveCamera(
   75, 
   window.innerWidth / window.innerHeight,
@@ -13,13 +11,12 @@ const camera = new THREE.PerspectiveCamera(
 );
 camera.position.z = 5;
 
-// Renderizador
+
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-// Función para obtener un color aleatorio
 function getRandomColor() {
   return new THREE.Color(Math.random(), Math.random(), Math.random());
 }
@@ -33,7 +30,6 @@ const materials = [
   () => new THREE.MeshNormalMaterial(),
 ];
 
-// Geometría y primer material
 const geometry = new THREE.BoxGeometry();
 let material = materials[Math.floor(Math.random() * materials.length)]();
 let cube = new THREE.Mesh(geometry, material);
@@ -44,21 +40,18 @@ const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(1, 1, 2);
 scene.add(light);
 
-// Evento de redimensionamiento
+//ventana resize
 window.addEventListener('resize', () => {
-  // Ajustar el tamaño del renderizador y cámara
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 
-  // Eliminar el material anterior (liberar memoria)
   cube.material.dispose();
 
-  // Asignar un nuevo material aleatorio
   cube.material = materials[Math.floor(Math.random() * materials.length)]();
 });
 
-// Animación
+
 function animate() {
   requestAnimationFrame(animate);
   cube.rotation.x += 0.01;
